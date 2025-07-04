887a3f06c81ab23f95ae114e509e7d496abce099
Amin Ghasemi <mmnghasemi@gmail.com>
2025-07-04 14:46:46 +0330

    Employees and Customers are created using SqlCommand , after first run Comment all SqlCommands

--- Program.cs
+++ Program.cs
@@ -9,10 +9,34 @@ namespace TablesOutputSDA
             // Create a new table using SqlCommand , Insert Data and Select using SqlDataAdapter
 
             string connectionString = """
-                Data Source =.; Initial Catalog = TestDB; User ID = sa; Encrypt = False
+                Data Source =.; Initial Catalog = TestDB; User ID = sa; Password = amin5123 ; Encrypt = False
                 """;
 
             SqlConnection connection = new SqlConnection(connectionString);
+            
+            SqlCommand CreateTableEmployees = new SqlCommand("""
+                CREATE TABLE Employees (
+                    Id INT PRIMARY KEY IDENTITY(1,1),
+                    Name NVARCHAR(20),
+                    Position NVARCHAR(50)
+                );
+                """, connection);
+
+            SqlCommand CreateTableCustomers = new SqlCommand("""
+                CREATE TABLE Customers (
+                    Id INT PRIMARY KEY IDENTITY(1,1),
+                    Name NVARCHAR(20),
+                    Address NVARCHAR(100)
+                );
+                """, connection);
+
+            connection.Open();
+            Console.WriteLine(CreateTableEmployees.ExecuteNonQuery());
+            Console.WriteLine(CreateTableCustomers.ExecuteNonQuery());
+            connection.Close();
+
+
+
         }
     }
 }
