0d65b2235b3d2528b9e2175df27f2e1b07d2719f
Amin Ghasemi <mmnghasemi@gmail.com>
2025-07-04 14:27:36 +0330

    Initial Commit

--- Program.cs
+++ Program.cs
@@ -4,6 +4,8 @@
     {
         static void Main(string[] args)
         {
+
+            // Create a new table using SqlCommand , Insert Data and Select using SqlDataAdapter
             Console.WriteLine("Hello, World!");
         }
     }
